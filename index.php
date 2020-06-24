b2a836f3c38a234fb7bb85d79f738149cc3bc7e5
Attache <attacheaka66@gmail.com>
2020-06-24 20:12:18 +0000

    insert caro

--- index.php
+++ index.php
@@ -5,22 +5,88 @@
 </div>
 
 <div class="container-fluid" style="padding:0;">
-	 <div class="row">
+	 <div class="row mr-md-2 mr-lg-2 mb-5">
 	 	   <div class="col-md-9 col-lg-9">
 	 	   	 <img class="img-fluid" src="dist/img/ministere.png" alt="">
 	 	   </div>
 			 <div class="col-md-3 col-lg-3" style="width:100%">
 				  <center>
-						<span style="color:#294A70;font-size:0.8em;"> IEPP Cocody Deux-Plateaux </span> <br>
-						<span style="color:#294A70;font-size:0.5em;"> Inspection de L'Enseignement Préscolaire et Primaire </span>
+						<span style="color:#294A70;font-size:1em;"> IEPP Cocody Deux-Plateaux </span> <br>
+						<span style="color:#294A70;font-size:0.7em;"> Inspection de L'Enseignement Préscolaire et Primaire </span>
 				 	  <div class="row">
-				 	  	 <div class="col-md-6 col-lg-6"> <img class="img-fluid" src="dist/img/menet@2x.png" alt=""> </div>
-							 <div class="col-md-6 col-lg-6"> <img class="img-fluid" src="dist/img/iepp@2x.png" alt=""> </div>
+				 	  	 <div class="col-sm-6 col-md-6 col-lg-6"> <img class="img-fluid" src="dist/img/menet@2x.png" alt=""> </div>
+							 <div class="col-sm-6 col-md-6 col-lg-6"> <img class="img-fluid" src="dist/img/iepp@2x.png" alt=""> </div>
 				 	  </div>
 					</center>
 			 </div>
 	 </div>
+ <!-- barre de navigation -->
+		 <nav class="navbar navbar-expand-lg navbar-light" style="background-color:#294A70;padding:0px">
+			 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				 <span class="navbar-toggler-icon"></span>
+			 </button>
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav m-auto text-center">
+		      <span style="background-color:#EB9075;" class="p-2 nav-item">
+		        <a class="nav-link" href="#"> <img src="dist/icons/home.svg" alt=""></a>
+		      </span>
+		      <li class="nav-item mb-2">
+		        <a class="nav-link text-white" href="#"> SERVICES </a>
+		      </li>
+					<li class="nav-item">
+		        <a class="nav-link text-white" href="#"> PROGRAMMES </a>
+		      </li>
+					<li class="nav-item">
+		        <a class="nav-link text-white" href="#"> EXAMENS - CONCOURS </a>
+		      </li>
+					<li class="nav-item">
+		        <a class="nav-link text-white" href="#"> MEDIA </a>
+		      </li>
+					<li class="nav-item">
+		        <a class="nav-link text-white" href="#"> ECOLE VIRTUELLE </a>
+		      </li>
+					<li class="nav-item">
+		        <a class="nav-link text-white" href="#"> DATABASE </a>
+		      </li>
+					<li class="nav-item">
+		        <a class="nav-link text-white" href="#"> SIG IEPP </a>
+		      </li>
+		    </ul>
+		  </div>
+		</nav>
+
+		<!-- carousel -->
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+		  <ol class="carousel-indicators">
+		    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+		    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+		    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		  </ol>
+		  <div class="carousel-inner">
+		    <div class="carousel-item active">
+		      <img src="dist/img/ministre.png" class="d-block w-100" alt="...">
+		    </div>
+		    <div class="carousel-item">
+		      <img src="dist/img/ministre.png" class="d-block w-100" alt="...">
+		    </div>
+		    <div class="carousel-item">
+		      <img src="dist/img/ministre.png" class="d-block w-100" alt="...">
+		    </div>
+		  </div>
+		  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+		    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+		    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>
+		<!-- flash info -->
+
 </div>
+
 
 
 
